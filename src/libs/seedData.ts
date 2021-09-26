438c95f8dd76941bc911acb7fc53f3e4f5a1be21
Manish Verma <manish.verma@manishr133.successivetech.com>
2021-09-26 23:58:00 +0530

    Day 13 - Models setup with initial Seed

---
+++ src/libs/seedData.ts
@@ -0,0 +1,21 @@
+import UserRepository from "src/repositories/user/UserRepository";
+//import UserRepository from "src/repositories/user/UserRepository";
+
+const UserRepository: UserRepository = new UserRepository();
+export default () => {
+    UserRepository.count()
+    .then(res => {
+        console.log('res', typeof res);
+          
+        if(res===0){
+            console.log('data seed in progress');
+            UserRepository.create({ name: 'manish', role: 'trainee', email: 'trainee@successive.tech', password: 'Trainee@123'});
+    
+        
+        }
+    
+    
+    
+   
+    }.catch(err => console.log(err));
+}
