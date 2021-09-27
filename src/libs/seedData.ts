a47818f669b9d1a6ea5f4313a0ec96eee007526c
Manish Verma <manish.verma@manishr133.successivetech.com>
2021-09-27 20:08:52 +0530

    Day 16 - Magic of Await

--- src/libs/seedData.ts
+++ src/libs/seedData.ts
@@ -1,21 +1,23 @@
-import UserRepository from "src/repositories/user/UserRepository";
-
+import UserRepository from '../repositories/user/UserRepository';
+import config from '../config/configuration'
+import * as bcrypt from 'bcrypt';
+import * as constants from '../libs/constants'
 
 const UserRepository: UserRepository = new UserRepository();
 export default () => {
     UserRepository.count()
-    .then(res => {
-        console.log('res', typeof res);
-          
-        if(res===0){
-            console.log('data seed in progress');
-            UserRepository.create({ name: 'manish', role: 'trainee', email: 'trainee@successive.tech', password: 'Trainee@123'});
-    
-        
-        }
-    
-    
-    
-   
-    }.catch(err => console.log(err));
+        .then(res => {
+            if (res === 0) {
+                console.log('data seed in progress');
+
+                bcrypt.hash(config.password, constants.BCRYPT_SALT_ROUNDS).then(function (hash) {
+                    UserRepository.create({ name: 'manish', role: 'trainee', email: 'trainee@successive.tech', password: hash });    //  password: 'Trainee@123'
+
+
+                });
+
+            }
+
+        })
+        .catch(err => console.log(err));
 }
